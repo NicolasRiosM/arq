0f976e858c3277803e838d8335f3c944fde4fcb3
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-10 17:35:00 -0400

    s

--- interfaz2.py
+++ interfaz2.py
@@ -55,7 +55,7 @@ while True:
         mensaje = aux + 'login' + datos
         
         socket.send(bytes(mensaje,'utf-8'))
-        if socket.recv(4096).find("ok"):
+        if socket.recv(4096).find('ok'):
                 print("Ha iniciado sesion con exito")
                 break
         else:
