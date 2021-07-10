6b08097ca1d9a30199995dc2de9deeca3964e996
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-10 17:33:54 -0400

    s

--- interfaz2.py
+++ interfaz2.py
@@ -55,7 +55,11 @@ while True:
         mensaje = aux + 'login' + datos
         
         socket.send(bytes(mensaje,'utf-8'))
-        
+        if socket.recv(4096).find("ok"):
+                print("Ha iniciado sesion con exito")
+                break
+        else:
+                print("No pudo iniciar sesion, intente nuevamente")
 
         
     if(opcion == '2'):
