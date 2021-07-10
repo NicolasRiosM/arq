c655a519fa2a701af836d37b1c289c88143398d7
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-10 18:31:20 -0400

    s

--- 1.- login/sv_login.py
+++ 1.- login/sv_login.py
@@ -5,21 +5,22 @@ import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
 
-s.bind(("localhost", 5000))
+s.connect(("localhost", 5000))
 s.send(bytes('00010sinitlogin','utf-8'))
 recibido=s.recv(4096)
+print(recibido)
 
-def recibir(sock, addr):
-    print("Ingresando a la cuenta de usuario")
-    while True:
-        datos = sock.recv(4096)
-        if datos.decode('utf-8').find('login'):
-            datos = datos[10:]
-            target = datos.decode()
-            data = target.split()          
+print("Ingresando a la cuenta de usuario")
+while True:
+    datos = s.recv(4096)
+    if datos.decode('utf-8').find('login'):
+        datos = datos[10:]
+        target = datos.decode()
+        data = target.split()
+
         consulta = f"select (email, pass) from usuario"
         respuesta = consultar(consulta)
-    #--------------------------------------------------------------#
+#--------------------------------------------------------------#
         i=0
         print("hola")
         while (i<len(respuesta)):
@@ -42,25 +43,22 @@ def recibir(sock, addr):
             psw.pop(0)
             psw.pop()
             password=''.join(psw)
-    #-------------------------VALIDANDO--------------------------#
-            enchash = password.encode()
-            encpass = data[1].encode()
-            val=0
-            if data[0] == email and bcrypt.checkpw(encpass, enchash):
-                val=1
-                print("Ha ingresado con éxito a su cuenta")
-                dat="ok"
-                mens= dat + " "
-                aux=llenado(len(mens+'login'))
-                mensaje=aux+'login'+mens
-                socket.send(bytes(mensaje,'utf-8'))
-                break
-    #--------------------------------------------------------------#
-    if (val!=1):
-        print("Contraseña incorrecta")
-        
-    s.close()
-while True:
-	sock, addr = s.accept()
-	tarea = threading.Thread(target = recibir, args = (sock, addr))
-	tarea.start()
+#-------------------------VALIDANDO--------------------------#
+        enchash = password.encode()
+        encpass = data[1].encode()
+        val=0
+        if data[0] == email and bcrypt.checkpw(encpass, enchash):
+            val=1
+            print("Ha ingresado con éxito a su cuenta")
+            dat="ok"
+            mens= dat + " "
+            aux=llenado(len(mens+'login'))
+            mensaje=aux+'login'+mens
+            socket.send(bytes(mensaje,'utf-8'))
+            break
+#--------------------------------------------------------------#
+if (val!=1):
+    print("Contraseña incorrecta")
+    
+s.close()
+
