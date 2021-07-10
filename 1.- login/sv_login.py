deb6c0e539279302f248c465d3b2c7adf68b8dd6
NicolasRiosM <69985870+NicolasRiosM@users.noreply.github.com>
2021-07-10 18:11:44 -0400

    s

--- 1.- login/sv_login.py
+++ 1.- login/sv_login.py
@@ -4,6 +4,7 @@ import bcrypt
 import threading
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(("localhost", 5000))
 s.send(bytes('00010sinitlogin','utf-8'))
 recibido=s.recv(4096)
